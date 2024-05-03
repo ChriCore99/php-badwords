55fff34ed9ce119acffb309fe17223764b28350d
ChriCore99 <christian.corelli.io@gmail.com>
2024-05-03 15:44:32 +0200

    ADD: stampe richieste dei valori delle parole

--- index.php
+++ index.php
@@ -20,8 +20,8 @@
 
         <form action="./server.php" method="POST">
         <p>
-                <label for="parolaCensurata">parola censurata</label>
-                <input type="text" name="parolaCensurata" placeholder="inserisci una parola">
+                <label for="parola_da_censurare">parola censurata</label>
+                <input type="text" name="parola_da_censurare" id="parola_da_censurare" placeholder="inserisci una parola">
             </p>
 
             <button>invia</button>
--- server.php
+++ server.php
@@ -1,11 +1,39 @@
 <?php 
+    // richiesta con metodo GET
     var_dump($_GET);
 
     $paragrafo = $_GET['paragrafo'];
 
-
+    // richiesta con metodo POST
     var_dump($_POST);
 
-    $parolaCensurata = $POST['parolaCensurata'];
+    $parola_da_censurare = $_POST['parola_da_censurare'];
+
+    $lunghezzaParolaBrutta = strlen($parola_da_censurare);
+
+    $parolaCensurata = str_replace($parola_da_censurare, '***', $parola_da_censurare);
+
+    $lunghezzaParolaCensurata = strlen($parolaCensurata); 
+
+    $carateriCambiati = $lunghezzaParolaBrutta - $lunghezzaParolaCensurata;
 ?>
 
+<div>
+    parola da censurare = <?php echo $parola_da_censurare; ?>
+</div>
+
+<div>
+    lunghezza parola da censurata = <?php echo $lunghezzaParolaBrutta; ?>
+</div>
+
+<div>
+    la parola censurata è = <?php echo $parolaCensurata; ?>
+</div>
+
+<div>
+    lunghezza parola censurata = <?php echo $lunghezzaParolaCensurata; ?>
+</div>
+
+<div>
+    i caratteri cambiati sono = <?php echo $carateriCambiati; ?>
+</div>
