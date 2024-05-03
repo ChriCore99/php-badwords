4ca428d258ec3aa572040f2e8939a7803bd44efa
ChriCore99 <christian.corelli.io@gmail.com>
2024-05-03 15:00:15 +0200

    ADD: prima aggiunta form

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <main>
+        <form action="">
+        <p>
+                <label for="paragrafo">paragrafo</label>
+                <input type="text" name="paragrafo" placeholder="inserisci un paragrafo">
+            </p>
+
+            <p>
+                <label for="parolaCensurata">parola censurata</label>
+                <input type="text" name="parolaCensurata" placeholder="inserisci una parola">
+            </p>
+
+            <button>invia</button>
+        </form>
+    </main>
+
+</body>
+</html>
